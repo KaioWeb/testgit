19b707e16f087165ee57313717137fdd89454b0e
KaioWeb <kaio.webdesigner@gmail.com>
2021-12-29 16:22:18 -0300

    test15

--- texto.js
+++ texto.js
@@ -1,3 +1,22 @@
 function nova(){
-    
+    //teste-3
+    let a = 3;
+    let b = 4;
+    let bestera;
+    let bestera2;
+    return a*b;
+}
+
+function nova2(){
+    //teste-nova2
+    let nova2;
+    let nova2;
+    return a*b;
+}
+
+function nova3(){
+    //teste-nova3
+    let nova3;
+    let nova3;
+    return a*b;
 }
